58f20aa48354a2b36363b77c44df10c51b7e913c
Compiler Praktikum <compiler@compiler-laptop.(none)>
2013-03-29 23:34:27 +0100

    Symboltabelle bearbeitet

--- src/symboltable.c
+++ src/symboltable.c
@@ -1,6 +1,9 @@
 #ifndef SYMBOLTABLE_H_
 #define SYMBOLTABLE_H_
 
+/* Daran kann man sich orientieren:
+ * http://www.gnu.org/software/bison/manual/html_node/Mfcalc-Symbol-Table.html
+ */
 
 enum type {
     INTEGER, VOID
@@ -41,29 +44,31 @@ scope_list_t function_scope = 0;
 
 
 
-int insertVariable(char* name){
+int insertVariable(int type,char* name){
     symbol_t myVar = {INTEGER, nextRegister, name};
     scope_list_t* newElement;
     
     malloc(newElement, sizeof(scope_list_t));
     newElement.sym = myVar;
 
-    scope_list_t global_list;
-    global_list = global_scope.next;
-    while(global_list != 0){
-        global_list = global_list.next;
-    } //am Ende von global_list angekommen
+    scope_list_t *current_scope;
 
-    // noch unfertig
-    if(function_scope != 0){ //füge Variable in den Scope der Funktion ein
-    	global_list.next = newElement;
+    if(function_scope == 0){ //füge Variable in den Scope der Funktion ein
+    	current_scope = global_scope;
 
     }else{ //Füge Variable in den globalen Scope ein
-    	global_scope.next = newElement;
+    	current_scope = function_scope;
 
     }
 
-    // ToDo: Pointer auf Scopelist setzen und Variable in die Scopelist einfügen
+    current_scope = current_scope.next;
+    do {
+        current_scope = current_scope.next;
+    }while(current_scope != 0); //am Ende von global_list angekommen
+
+
+
+   //Pointer auf Scopelist setzen und Variable in die Scopelist einfügen
 
     return newElement; //Pointer auf neue Variable zurückgeben
 
